ef5b9fd52d23fe295689f6a61e4a0bc6231c4ac2
Sahil Kotian <sahil.kotian316@gmail.com>
2024-08-20 13:11:30 -0400

    Implemented Drag-And-Drop File Selection Functionality

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,10 +1,24 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-import { useState } from 'react'
+import { Image, Loader2, MousePointerSquareDashed } from 'lucide-react'
+import { useState, useTransition } from 'react'
+import Dropzone from 'react-dropzone'
 
 const Page = () => {
   const [isDragOver, setIsDragOver] = useState<boolean>(false)
+  const isUploading = false
+
+  const onDropRejected = () => {
+    setIsDragOver(false)
+  }
+
+  const onDropAccepted = () => {
+    console.log("File Accepted!")
+    setIsDragOver(false)
+  }
+
+  const [isPending, startTransition] = useTransition()
 
   return (
     <div
@@ -14,6 +28,33 @@ const Page = () => {
           'ring-blue-900/25 bg-blue-900/10': isDragOver,
         }
       )}>
+      <div className='relative flex flex-1 flex-col items-center justify-center w-full'>
+        <Dropzone
+          onDropRejected={onDropRejected}
+          onDropAccepted={onDropAccepted}
+          accept={{
+            'image/png': ['.png'],
+            'image/jpeg': ['.jpeg'],
+            'image/jpg': ['.jpg'],
+          }}
+          onDragEnter={() => setIsDragOver(true)}
+          onDragLeave={() => setIsDragOver(false)}>
+          {({ getRootProps, getInputProps }) => (
+            <div
+              className='h-full w-full flex-1 flex flex-col items-center justify-center'
+              {...getRootProps()}>
+              <input {...getInputProps()} />
+              {isDragOver ? (
+                <MousePointerSquareDashed className='h-6 w-6 text-zinc-500 mb-2' />
+              ) : isUploading || isPending ? (
+                <Loader2 className='animate-spin h-6 w-6 text-zinc-500 mb-2' />
+              ) : (
+                <Image className='h-6 w-6 text-zinc-500 mb-2' />
+              )}
+            </div>
+          )}
+        </Dropzone>
+      </div>
     </div>
   )
 }
