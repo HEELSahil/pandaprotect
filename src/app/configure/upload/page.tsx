976a50519722106125a65cb2ed5144a41a3cd7b9
Sahil Kotian <sahil.kotian316@gmail.com>
2024-08-20 13:46:50 -0400

    Added Progress Bar And Upload Status To File Selection Functionality

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Progress } from '@/components/ui/progress'
 import { cn } from '@/lib/utils'
 import { Image, Loader2, MousePointerSquareDashed } from 'lucide-react'
 import { useState, useTransition } from 'react'
@@ -7,6 +8,8 @@ import Dropzone from 'react-dropzone'
 
 const Page = () => {
   const [isDragOver, setIsDragOver] = useState<boolean>(false)
+  const [uploadProgress, setUploadProgress] = useState<number>(0)
+  
   const isUploading = false
 
   const onDropRejected = () => {
@@ -50,6 +53,33 @@ const Page = () => {
                 <Loader2 className='animate-spin h-6 w-6 text-zinc-500 mb-2' />
               ) : (
                 <Image className='h-6 w-6 text-zinc-500 mb-2' />
+              )}
+              <div className='flex flex-col justify-center mb-2 text-sm text-zinc-700'>
+                {isUploading ? (
+                  <div className='flex flex-col items-center'>
+                    <p>Uploading...</p>
+                    <Progress
+                      value={uploadProgress}
+                      className='mt-2 w-40 h-2 bg-gray-300'
+                    />
+                  </div>
+                ) : isPending ? (
+                  <div className='flex flex-col items-center'>
+                    <p>Redirecting, please wait...</p>
+                  </div>
+                ) : isDragOver ? (
+                  <p>
+                    <span className='font-semibold'>Drop file</span> to upload
+                  </p>
+                ) : (
+                  <p>
+                    <span className='font-semibold'>Click to upload</span> or drag and drop
+                  </p>
+                )}
+              </div>
+
+              {isPending ? null : (
+                <p className='text-xs text-zinc-500'>PNG, JPG, JPEG</p>
               )}
             </div>
           )}
